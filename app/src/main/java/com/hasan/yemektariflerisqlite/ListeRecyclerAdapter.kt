96f5bb395c64faa98199fe8f77e390ab31e9c2f1
Your Name <you@example.com>
2021-03-12 00:29:44 +0300

    first commit

---
+++ app/src/main/java/com/hasan/yemektariflerisqlite/ListeRecyclerAdapter.kt
@@ -0,0 +1,35 @@
+package com.hasan.yemektariflerisqlite
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_liste.view.*
+import kotlinx.android.synthetic.main.recycler_row.view.*
+
+class ListeRecyclerAdapter(val yemekListesi: ArrayList<String>, val idListesi: ArrayList<Int> ) : RecyclerView.Adapter<ListeRecyclerAdapter.YemekHolder>() {
+
+    class YemekHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): YemekHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.recycler_row,parent,false)
+        return YemekHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: YemekHolder, position: Int) {
+        holder.itemView.recycler_row_text.text= yemekListesi[position]
+        holder.itemView.setOnClickListener {
+            val action = ListeFragmentDirections.actionListeFragmentToTarifFragment("recyclerdangeldim",idListesi[position])
+            Navigation.findNavController(it).navigate(action)
+        }
+     }
+
+    override fun getItemCount(): Int {
+        return yemekListesi.size
+    }
+}
